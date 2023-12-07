4b8195044f760df6fa8678920dbe0cabb161348f
Mislav Giljanovic <giljanovicmislav@gmail.com>
2023-12-07 22:13:42 +0100

    Master

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 import Input from "./Components/Input";
 import ApiPoziv from "./Components/ApiPoziv";
 import Display from "./Components/Display";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  const [korisnickoIme, setKorisnickoIme] = useState("");
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { korisnickoIme: "" };
+    this.handleFetchData = this.handleFetchData.bind(this);
+  }
 
-  const handleFetchData = (korisnickoIme) => {
-    setKorisnickoIme(korisnickoIme);
-  };
+  handleFetchData(korisnickoIme) {
+    this.setState({ korisnickoIme: korisnickoIme });
+  }
 
-  return (
-    <div className="App">
-      <Container className="d-flex align-items-center justify-content-center flex-column">
-        <Input onFetchData={handleFetchData} />
-        <ApiPoziv korisnickoIme={korisnickoIme} />
-        <Display />
-      </Container>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Container className="d-flex align-items-center justify-content-center flex-column">
+          <Input onFetchData={this.handleFetchData} />
+          <ApiPoziv korisnickoIme={this.state.korisnickoIme} />
+          <Display />
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
+
 
